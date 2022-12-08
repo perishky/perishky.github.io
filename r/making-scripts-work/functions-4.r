44726c33bd803e7c33acfcce79e2544386b41a8f
Matthew Suderman <matthewsuderman@hotmail.com>
2022-12-08 23:21:52 +0000

    Create functions-4.r

---
+++ r/making-scripts-work/functions-4.r
@@ -0,0 +1,8 @@
+reduce.dimensions <- function(variables, effects, num) {
+    variables <- remove.constants(variables) 
+    stopifnot(num <= ncol(variables)) ###### added to catch cases where 'num' is too large
+    variables <- scale(variables)
+    variables <- remove.effects(variables, effects)
+    reduced.variables <- compute.pcs(variables, num)
+    reduced.variables
+}
