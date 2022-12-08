e4a7cc672946ed4ae8d8280321474772a56c3c16
Matthew Suderman <matthewsuderman@hotmail.com>
2022-12-08 23:53:54 +0000

    Update functions-3.r

--- r/making-scripts-work/functions-3.r
+++ r/making-scripts-work/functions-3.r
@@ -3,9 +3,10 @@ source("functions-2.r")
 remove.constants <- function(variables) {
     ss <- rep(NA,ncol(variables))
     for (i in 1:ncol(variables))
-        ss[i] <- var(variables[,i], na.rm=T) ###### added na.rm=T
+        ss[i] <- var(variables[,i])
+    browser()                               #### start the code browser
     is.constant <- ss < 2e-16
-    if (any(is.constant)) {
+    if (any(is.constant)) {                 ## where the error was generated
         warning("Omitting variables with zero variance: ", sum(is.constant))
         variables <- variables[,!is.constant,drop=F]
     }
