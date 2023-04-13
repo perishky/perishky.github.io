c9e98a8bd90d6542a28fc1c04cbca854d4275981
Matthew Suderman <matthewsuderman@hotmail.com>
2023-04-14 00:27:13 +0100

    how-to-wow

---
+++ r/how-to-wow/how-to-wow.r
@@ -0,0 +1,14 @@
+## ---- options -----------------
+knitr::opts_chunk$set(
+  fig.align = "center",
+  fig.dpi = 320,
+  fig.height = 5,
+  fig.width = 5,
+  dev="svg",
+  message = FALSE,
+  warning = FALSE,
+  collapse = TRUE,
+  ##cache = TRUE, cache.lazy = FALSE,
+  comment = "#>")
+options(htmltools.dir.version = FALSE)
+
